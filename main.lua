6c17f908bafb5050daf594f3a0fe398ded00afdc
Zachariah Bauermeister <zac@bauermeister.com>
2020-07-15 13:18:42 -0700

    removed draw calls

--- main.lua
+++ main.lua
@@ -3,18 +3,17 @@ function love.load()
 	host = enet.host_create("*:6789")
 	peers = {}
 	single = nil
-	logs = {}
 end
 
 function love.update(dt)
 	local event = host:service(100)
 	while event do
 	  if event.type == "connect" then
-	  	log(event.peer:connect_id().." connected!")
+	  	print(event.peer:connect_id().." connected!")
 	    if single == nil then
 	    	single = event.peer
 	    else
-	    	log("connecting "..event.peer:connect_id().." to "..single:connect_id()..".")
+	    	print("connecting "..event.peer:connect_id().." to "..single:connect_id()..".")
 	    	peers[single] = event.peer
 	    	peers[event.peer] = single
 	    	event.peer:send("host")
@@ -26,17 +25,4 @@ function love.update(dt)
 	  end
 	  event = host:service()
 	end
-end
-
-function log(message)
-	logs[#logs+1] = message
-end
-
-function love.draw()
-	local logMessage = ""
-	local firstLog = math.max(1, #logs - 10)
-	for i=firstLog,#logs do
-		logMessage = logMessage..logs[i].."\n"
-	end
-	love.graphics.print(logMessage, 0, 0)
 end
